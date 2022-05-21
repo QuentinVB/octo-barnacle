79c91a2400f5de85fe2c25af9ad8d1acc0e2948b
Thomas bergon <bergon.thomas@hotmail.fr>
2022-05-22 00:17:13 +0200

    Dash pas à travers les murs

--- GameJam/Assets/Player.cs
+++ GameJam/Assets/Player.cs
@@ -6,7 +6,7 @@ using UnityEngine.Sprites;
 public class Player : MonoBehaviour
 {
     public float speed=9f;
-    public float jumpSpeed=20000f;
+    public float jumpSpeed=200f;
     private int sneaked=1;
     public GameObject player;
     public Collider coll;
@@ -14,6 +14,7 @@ public class Player : MonoBehaviour
     public List<Vector3> respawnPoint=new List<Vector3>();
     private int faceR=1;
     private bool jumped=false;
+    bool grounded=false;
     // Start is called before the first frame update
 
     [SerializeField]
@@ -21,8 +22,6 @@ public class Player : MonoBehaviour
 
     void Start()
     {
-        respawnPoint.Add(new Vector3(738.08f,1.42f,-800.0405f));
-        Physics.gravity=new Vector3(0, -1.0F, 0);
         player.name="bob"; // temp name
         coll=GetComponent<Collider>();
         coll.isTrigger = false;
@@ -37,9 +36,18 @@ public class Player : MonoBehaviour
         rigidbody.AddForce(new Vector3(0,-9.81f,0) * rigidbody.mass);
     }
     private void OnCollisionEnter(Collision other) {
-        Debug.Log("test");
         if(other.gameObject.name=="Death"){Respawn(0);}
+        Collider[] hitColliders = Physics.OverlapSphere(rigidbody.position, 1f);
+        grounded=false;
+        foreach(Collider collid in hitColliders){
+            if(collid.transform.position.y < rigidbody.position.y){grounded=true;}
+            // else if(collid.transform.position.x - rigidbody.position.x<0 && faceR==-1 ){grounded=true;}
+            // else if(collid.transform.position.x - rigidbody.position.x<0.5 && faceR==1 ){
+            //     Debug.Log(collid.transform.position.x - rigidbody.position.x);
+            //     grounded=true;}
+        }
         jumped=false;
+        //Debug.Log("grounded : " + grounded);
     }
     // Update is called once per frame
     void Update()
@@ -55,27 +63,46 @@ public class Player : MonoBehaviour
         float horizontal=0f;
         float jump=0f;
         horizontal = Input.GetAxis("Horizontal") * speed; // negatif gauche , positif droite
-        faceR= Input.GetAxis("Horizontal")>0 ? 1 :  ((Input.GetAxis("Horizontal")==0) ? faceR : -1);
+        faceR= Input.GetAxis("Horizontal")>0 ? -1 :  ((Input.GetAxis("Horizontal")==0) ? faceR : 1);
 
-        if(Input.GetAxis("Vertical")>0 && rigidbody.velocity.y<0f &&!jumped){
+        if(Input.GetKeyDown("d")){horizontal=Dash(1);}
+        else if(Input.GetKeyDown("q")){horizontal=Dash(-1);}
+        else if(Input.GetAxis("Vertical")>0 && grounded){
+            Debug.Log("Jump");
             jump=jumpSpeed;
             jumped=true;
-            Debug.Log("lkgdfjnlkg");
+            grounded=false;
         }
         if(Input.GetAxis("Vertical")<0){
             sneaked=2;
             return;
         }
-        if(Input.GetKeyDown("space")){ //Dash
-            Debug.Log("Dash");
-            horizontal=faceR * 2000f * Time.deltaTime;
+        if(Input.GetKeyDown("space")){ //jump
+            //horizontal=speed *faceR * 2000f * Time.deltaTime;
+            jump=jumpSpeed;
         }
-        Vector3 vec=new Vector3(horizontal*Time.deltaTime/sneaked,jump*Time.deltaTime/sneaked,0.0f);
+        Vector3 vec=new Vector3(-1f*horizontal*Time.deltaTime/sneaked,jump*Time.deltaTime,0.0f);
         transform.Translate(vec);
 
     }
 
     public void Respawn(int currentLevel){
         rigidbody.position=respawnPoint[currentLevel];
+    }
+    public float Dash(int direction){
+        Collider[] hitColliders = Physics.OverlapSphere(rigidbody.position, 3.6f);
+        foreach(Collider collid in hitColliders){
+            if(collid.transform.position!=rigidbody.transform.position){
+                if(direction==-1 && collid.transform.position.x<rigidbody.transform.position.x){
+                    Debug.Log(" pas Dash yeah!");
+                    return 0.0f;
+                }
+                else if(direction==1 && collid.transform.position.x>rigidbody.transform.position.x){
+                    Debug.Log("pas Dash yeah!");
+                    return 0.0f;
+                }
+            }
+        }
+        return speed *direction * 2000f * Time.deltaTime;
     }
 }
