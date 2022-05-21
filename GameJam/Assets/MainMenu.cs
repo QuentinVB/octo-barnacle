b661068fb14694a74df2d317dae81a1b3e7f4b87
Snowky92 <amaurypinheiro@hotmail.fr>
2022-05-21 14:59:50 +0200

    Ajout du début du menu pause

--- GameJam/Assets/MainMenu.cs
+++ GameJam/Assets/MainMenu.cs
@@ -4,13 +4,18 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public string levelToLoad;
+    public GameObject settingsWindow;
 
     public void StartGame(){
         SceneManager.LoadScene(levelToLoad);
     }
 
     public void SettingsButton(){
-        
+        settingsWindow.SetActive(true);
+    }
+
+    public void CloseSettingsButton(){
+        settingsWindow.SetActive(false);
     }
 
     public void QuitGame(){
--- GameJam/Assets/SettingsMenu.cs
+++ GameJam/Assets/SettingsMenu.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using System.Linq;
 
 public class SettingsMenu : MonoBehaviour
 {
@@ -12,17 +13,24 @@ public class SettingsMenu : MonoBehaviour
 
     public void Start(){
 
-        resolutions = Screen.resolutions;
+        resolutions = Screen.resolutions.Select(resolution => new Resolution { width = resolution.width, height = resolution.height}).Distinct().ToArray();
         resolutionDropdown.ClearOptions();
 
         List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
 
         for (int i = 0; i < resolutions.Length; i++){
             string option = resolutions[i].width + "x" + resolutions[i].height;
             options.Add(option);
+
+            if(resolutions[i].width == Screen.width && resolutions[i].height == Screen.height){
+                currentResolutionIndex = i;
+            }
         }
 
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
     public void SetVolume(float volume){
@@ -34,4 +42,8 @@ public class SettingsMenu : MonoBehaviour
         Screen.fullScreen = isFullScreen;
     }
 
+    public void SetResolution(int resolutionIndex){
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 }
